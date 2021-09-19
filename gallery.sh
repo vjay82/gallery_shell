158be521ee95257e5365bbaee7dbb75af6535de9
Volker Gronau <vjay@gmx.net>
2021-09-19 12:47:45 +0200

    HEIC handling; see README

--- gallery.sh
+++ gallery.sh
@@ -13,13 +13,16 @@ MY_HEIGHT_LARGE=768
 MY_QUALITY=85
 MY_THUMBDIR="__thumbs"
 MY_INDEX_HTML_FILE="index.html"
-MY_TITLE="Gallery"
-MY_FOOTER='Created with <a href="https://github.com/Cyclenerd/gallery_shell">gallery.sh</a>'
+MY_TITLE=${PWD##*/}
+MY_FOOTER='Created with <a href="https://github.com/vjay82/gallery_shell">gallery.sh</a>'
 
 # Use convert from ImageMagick
 MY_CONVERT_COMMAND="convert" 
 # Use JHead for EXIF Information
 MY_EXIF_COMMAND="jhead"
+HEIC_EXIF_COMMAND="exiftool"
+
+HEIC_TO_JPG_QUALITY=96
 
 # Bootstrap 4
 MY_CSS="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"
@@ -37,6 +40,13 @@ MY_SCRIPT_NAME=$(basename "$0")
 MY_DATETIME=$(date -u "+%Y-%m-%d %H:%M:%S")
 MY_DATETIME+=" UTC"
 
+function max6 {
+   while [ `jobs | wc -l` -ge 6 ]
+   do
+      sleep 1
+   done
+}
+
 function usage {
 	MY_RETURN_CODE="$1"
 	echo -e "Usage: $MY_SCRIPT_NAME [-t <title>] [-d <thumbdir>] [-h]:
@@ -48,7 +58,7 @@ function usage {
 
 function debugOutput(){
 	if [[ "$MY_DEBUG" == true ]]; then
-		echo "$1" # if debug variable is true, echo whatever's passed to the function
+		echo "$*" # if debug variable is true, echo whatever's passed to the function
 	fi
 }
 
@@ -87,6 +97,10 @@ debugOutput "- $MY_SCRIPT_NAME : $MY_DATETIME"
 ### Check Commands
 command -v $MY_CONVERT_COMMAND >/dev/null 2>&1 || { echo >&2 "!!! $MY_CONVERT_COMMAND it's not installed.  Aborting."; exit 1; }
 command -v $MY_EXIF_COMMAND >/dev/null 2>&1 || { echo >&2 "!!! $MY_EXIF_COMMAND it's not installed.  Aborting."; exit 1; }
+command -v rename >/dev/null 2>&1 || { echo >&2 "!!! rename it's not installed.  Aborting."; exit 1; }
+
+### Clean thumbs dir
+rm -rf "$MY_THUMBDIR"
 
 ### Create Folders
 [[ -d "$MY_THUMBDIR" ]] || mkdir "$MY_THUMBDIR" || exit 2
@@ -108,6 +122,11 @@ cat > "$MY_INDEX_HTML_FILE" << EOF
 	<meta name="viewport" content="width=device-width">
 	<meta name="robots" content="noindex, nofollow">
 	<link rel="stylesheet" href="$MY_CSS">
+<style>
+.btn {
+    margin-bottom: 5px;
+}
+</style>
 </head>
 <body>
 <header>
@@ -122,19 +141,49 @@ cat > "$MY_INDEX_HTML_FILE" << EOF
 <main class="container">
 EOF
 
+### Rename all files to their correct extensions
+rename 's/\.jpeg/\.jpg/' *.jpeg 2> /dev/null
+rename 's/\.JPG/\.JPGTMP/' *.JPG 2> /dev/null
+rename 's/\.JPGTMP/\.jpg/' *.JPGTMP 2> /dev/null
+rename 's/\.HEIC/\.HEICTMP/' *.HEIC 2> /dev/null
+rename 's/\.HEICTMP/\.heic/' *.HEICTMP 2> /dev/null
+rename 's/\.MOV/\.MOVTMP/' *.MOV 2> /dev/null
+rename 's/\.MOVTMP/\.mov/' *.MOVTMP 2> /dev/null
+
+### Convert heic to jpg
+if [[ $(find . -maxdepth 1 -type f -iname \*.heic | wc -l) -gt 0 ]]; then
+    for MY_FILENAME in *.heic; do
+        JPG_FILENAME=${MY_FILENAME%.heic}.jpg
+        if [ -f "$JPG_FILENAME" ]; then
+            debugOutput "Skipping converting heic to jpg: $MY_FILENAME"
+        else
+            debugOutput "Converting heic to jpg: $MY_FILENAME"
+            max6 ; $MY_CONVERT_COMMAND -auto-orient -quality $HEIC_TO_JPG_QUALITY -format jpg "$MY_FILENAME" "$JPG_FILENAME" &
+        fi
+    done
+    wait
+fi
+
 ### Photos (JPG)
 if [[ $(find . -maxdepth 1 -type f -iname \*.jpg | wc -l) -gt 0 ]]; then
 
 echo '<div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 py-5">' >> "$MY_INDEX_HTML_FILE"
-## Generate Images
+### Generate Thumbnail Images
 MY_NUM_FILES=0
-for MY_FILENAME in *.[jJ][pP][gG]; do
+for MY_FILENAME in *.jpg; do
 	MY_FILELIST[$MY_NUM_FILES]=$MY_FILENAME
 	(( MY_NUM_FILES++ ))
 	for MY_RES in ${MY_HEIGHTS[*]}; do
 		if [[ ! -s $MY_THUMBDIR/$MY_RES/$MY_FILENAME ]]; then
-			debugOutput "$MY_THUMBDIR/$MY_RES/$MY_FILENAME"
-			$MY_CONVERT_COMMAND -auto-orient -strip -quality $MY_QUALITY -resize x$MY_RES "$MY_FILENAME" "$MY_THUMBDIR/$MY_RES/$MY_FILENAME"
+			# prefer converting from the original if it has not been the jpg
+            MY_BASE_FILENAME=${MY_FILENAME%.jpg}
+	        HEIC_FILENAME=${MY_BASE_FILENAME}.heic
+			READ_FROM_FILE=$MY_FILENAME
+            if [ -f "$HEIC_FILENAME" ]; then
+                READ_FROM_FILE=$HEIC_FILENAME
+            fi
+            debugOutput converting "$READ_FROM_FILE" to "$MY_THUMBDIR/$MY_RES/$MY_FILENAME"
+            max6 ; $MY_CONVERT_COMMAND -auto-orient -strip -quality $MY_QUALITY -resize x$MY_RES "$READ_FROM_FILE" "$MY_THUMBDIR/$MY_RES/$MY_FILENAME" &
 		fi
 	done
 	cat >> "$MY_INDEX_HTML_FILE" << EOF
@@ -147,10 +196,12 @@ EOF
 done
 echo '</div>' >> "$MY_INDEX_HTML_FILE"
 
-## Generate the HTML Files for Images in thumbdir
+### Generate the HTML Files for Images in thumbdir
 MY_FILE=0
 while [[ $MY_FILE -lt $MY_NUM_FILES ]]; do
 	MY_FILENAME=${MY_FILELIST[$MY_FILE]}
+	MY_BASE_FILENAME=${MY_FILENAME%.jpg}
+	HEIC_FILENAME=${MY_BASE_FILENAME}.heic
 	MY_PREV=""
 	MY_NEXT=""
 	[[ $MY_FILE -ne 0 ]] && MY_PREV=${MY_FILELIST[$((MY_FILE - 1))]}
@@ -191,7 +242,7 @@ EOF
 	fi
 	cat >> "$MY_IMAGE_HTML_FILE" << EOF
 </div>
-<div class="col d-none d-md-block text-center"><h3>$MY_FILENAME</h3></div>
+<div class="col d-none d-md-block text-center"><h3>$MY_BASE_FILENAME</h3></div>
 <div class="col text-right">
 EOF
 	if [[ $MY_NEXT ]]; then
@@ -207,9 +258,23 @@ EOF
 		<p><img src="$MY_HEIGHT_LARGE/$MY_FILENAME" class="img-fluid" alt="Image: $MY_FILENAME"></p>
 	</div>
 </div>
-<div class="row">
+<div class="row" style="flex-direction: column">
+EOF
+
+	JPG_DOWNLOAD_TITLE="Download Original (JPEG Format)"
+	if [ -f "$HEIC_FILENAME" ]; then
+		JPG_DOWNLOAD_TITLE="Download Converted to JPEG Format"
+		HEIC_FILESIZE=$(getFileSize "$HEIC_FILENAME")
+		cat >> "$MY_IMAGE_HTML_FILE" << EOF
 	<div class="col">
-		<p><a class="btn btn-primary" href="../$MY_FILENAME">Download Original ($MY_FILESIZE)</a></p>
+		<p><a class="btn btn-primary" style="width:100%" href="../$HEIC_FILENAME">Download Original (HEIC Format) ($HEIC_FILESIZE)</a></p>
+	</div>
+EOF
+	fi
+
+	cat >> "$MY_IMAGE_HTML_FILE" << EOF
+	<div class="col">
+		<p><a class="btn btn-primary" style="width:100%" href="../$MY_FILENAME">$JPG_DOWNLOAD_TITLE ($MY_FILESIZE)</a></p>
 	</div>
 </div>
 EOF
@@ -218,8 +283,23 @@ EOF
 	if [[ $MY_EXIF_INFO ]]; then
 		cat >> "$MY_IMAGE_HTML_FILE" << EOF
 <div class="row">
+EOF
+
+		if [ -f "$HEIC_FILENAME" ]; then
+			HEIC_EXIF_INFO=$($HEIC_EXIF_COMMAND "$HEIC_FILENAME")
+			cat >> "$MY_IMAGE_HTML_FILE" << EOF
 <div class="col">
 <pre>
+<b>HEIC Exif:</b>
+$HEIC_EXIF_INFO
+</pre>
+</div>
+EOF
+		fi
+		cat >> "$MY_IMAGE_HTML_FILE" << EOF
+<div class="col">
+<pre>
+<b>JPEG Exif:</b>
 $MY_EXIF_INFO
 </pre>
 </div>
@@ -275,9 +355,14 @@ EOF
 fi
 
 ### Downloads (ZIP)
+rm "Images in HEIC Format.zip" "Images in JPEG Format.zip" "Movies in MOV Format.zip" 2> /dev/null
+zip "Images in HEIC Format.zip" *.heic &
+zip "Images in JPEG Format.zip" *.jpg &
+zip "Movies in MOV Format.zip" *.mov &
+wait
 if [[ $(find . -maxdepth 1 -type f -iname \*.zip | wc -l) -gt 0 ]]; then
 	cat >> "$MY_INDEX_HTML_FILE" << EOF
-	<div class="row">
+	<div class="row" style="margin-top:30px">
 		<div class="col">
 			<div class="page-header"><h2>Downloads</h2></div>
 		</div>
